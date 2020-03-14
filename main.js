7fc20eb08de6df6ccfa3df2bbd5867e4f15efd4a
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-03-14 10:20:35 +0100

    Added computed properties

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 var app = new Vue({
   el: "#app",
   data: {
-    product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    inStock: false,
+    brand: "VUE PRODUCT",
+    product: "WEB APP",
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester", "Gender-Neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -23,8 +25,19 @@ var app = new Vue({
     addToCart() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     }
   }
 })
